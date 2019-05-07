322e7cc0484331e156157bb347f8e59527a790f2
Tim Makkelie <timmakkelie@gmail.com>
2019-05-07 18:18:54 +0200

    added labels to form

--- textbasedrpg/textbasedrpg/Form1.Designer.cs
+++ textbasedrpg/textbasedrpg/Form1.Designer.cs
@@ -29,22 +29,99 @@
         private void InitializeComponent()
         {
             this.lblTimmy = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblHitPoints = new System.Windows.Forms.Label();
+            this.lblGold = new System.Windows.Forms.Label();
+            this.lblExperience = new System.Windows.Forms.Label();
+            this.lblLevel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblTimmy
             // 
             this.lblTimmy.AutoSize = true;
-            this.lblTimmy.Location = new System.Drawing.Point(286, 124);
+            this.lblTimmy.Location = new System.Drawing.Point(25, 25);
             this.lblTimmy.Name = "lblTimmy";
-            this.lblTimmy.Size = new System.Drawing.Size(35, 13);
+            this.lblTimmy.Size = new System.Drawing.Size(55, 13);
             this.lblTimmy.TabIndex = 0;
-            this.lblTimmy.Text = "label1";
+            this.lblTimmy.Text = "Hit Points:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Gold:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Experience:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Level:";
+            // 
+            // lblHitPoints
+            // 
+            this.lblHitPoints.AutoSize = true;
+            this.lblHitPoints.Location = new System.Drawing.Point(110, 25);
+            this.lblHitPoints.Name = "lblHitPoints";
+            this.lblHitPoints.Size = new System.Drawing.Size(35, 13);
+            this.lblHitPoints.TabIndex = 4;
+            this.lblHitPoints.Text = "label4";
+            // 
+            // lblGold
+            // 
+            this.lblGold.AutoSize = true;
+            this.lblGold.Location = new System.Drawing.Point(110, 46);
+            this.lblGold.Name = "lblGold";
+            this.lblGold.Size = new System.Drawing.Size(35, 13);
+            this.lblGold.TabIndex = 5;
+            this.lblGold.Text = "label5";
+            // 
+            // lblExperience
+            // 
+            this.lblExperience.AutoSize = true;
+            this.lblExperience.Location = new System.Drawing.Point(110, 67);
+            this.lblExperience.Name = "lblExperience";
+            this.lblExperience.Size = new System.Drawing.Size(35, 13);
+            this.lblExperience.TabIndex = 6;
+            this.lblExperience.Text = "label6";
+            // 
+            // lblLevel
+            // 
+            this.lblLevel.AutoSize = true;
+            this.lblLevel.Location = new System.Drawing.Point(110, 88);
+            this.lblLevel.Name = "lblLevel";
+            this.lblLevel.Size = new System.Drawing.Size(35, 13);
+            this.lblLevel.TabIndex = 7;
+            this.lblLevel.Text = "label7";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblLevel);
+            this.Controls.Add(this.lblExperience);
+            this.Controls.Add(this.lblGold);
+            this.Controls.Add(this.lblHitPoints);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.lblTimmy);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +133,13 @@
         #endregion
 
         private System.Windows.Forms.Label lblTimmy;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblHitPoints;
+        private System.Windows.Forms.Label lblGold;
+        private System.Windows.Forms.Label lblExperience;
+        private System.Windows.Forms.Label lblLevel;
     }
 }
 
