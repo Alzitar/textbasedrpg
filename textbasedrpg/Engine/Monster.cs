f708ef4ec0f335b779e62730caa9b0f62cbaf4fd
Tim Makkelie <timmakkelie@gmail.com>
2019-05-08 11:57:37 +0200

    added class inheritance and constructors.

--- textbasedrpg/Engine/Monster.cs
+++ textbasedrpg/Engine/Monster.cs
@@ -6,6 +6,15 @@ namespace Engine
 {
     public class Monster : LivingCreature
     {
+        public Monster(int currentHitpoints, int maximumHitpoints, int id, string name, int maximumDamage, int rewardExperiencePoints, int rewardGold) : base(currentHitpoints, maximumHitpoints)
+        {
+            ID = id;
+            Name = name;
+            MaximumDamage = maximumDamage;
+            RewardExperiencePoints = rewardExperiencePoints;
+            RewardGold = rewardGold;
+        }
+
         public int ID { get; set; }
         public string Name { get; set; }
         public int MaximumDamage { get; set; }
--- textbasedrpg/Engine/Player.cs
+++ textbasedrpg/Engine/Player.cs
@@ -6,6 +6,13 @@ namespace Engine
 {
     public class Player : LivingCreature
     {
+        public Player(int currentHitpoints, int maximumHitpoints, int gold, int experiencePoints, int level) : base(currentHitpoints, maximumHitpoints)
+        {
+            Gold = gold;
+            ExperiencePoints = experiencePoints;
+            Level = level;
+        }
+
         public int Gold { get; set; }
         public int ExperiencePoints { get; set; }
         public int Level { get; set; }
