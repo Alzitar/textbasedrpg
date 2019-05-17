7545fd8f0e4fc0808a122b0f86ff560e1d1b5ac3
Tim Makkelie <timmakkelie@gmail.com>
2019-05-17 13:14:21 +0200

    initial commit

--- SpaceGame/SpaceGame/SpaceGame/Properties/AssemblyInfo.cs
+++ SpaceGame/SpaceGame/SpaceGame/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8dbae8ea-048e-4c7b-afcd-3ac710069ab4")]
+[assembly: Guid("0e338ae2-35ae-4a0f-aa05-88dc8edc35be")]
 
 // Version information for an assembly consists of the following four values:
 //
